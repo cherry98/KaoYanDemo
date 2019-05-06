7bad8663b4d1a0a31f5c3d9fe97fa0f7022b4995
dongyan.xie <Dongyan.Xie@partner.bmw.com>
2019-05-06 18:37:10 +0800

    init

--- app/src/main/java/com/kaoyan/kaoyandemo/MainActivity.java
+++ app/src/main/java/com/kaoyan/kaoyandemo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.kaoyan.kaoyandemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.kaoyan.kaoyandemo.activity.BaseActivity;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
---
+++ app/src/main/java/com/kaoyan/kaoyandemo/activity/BaseActivity.java
@@ -0,0 +1,44 @@
+package com.kaoyan.kaoyandemo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
